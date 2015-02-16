3fb67c4467dd8c6a9b2c7002409cab96409ce6b2
Jérôme Mahuet <jerome.mahuet@gmail.com>
2015-02-16 22:15:41 +0100

    refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 
-import           Parser
-import           Evaluator
-import           System.Environment (getArgs)
+import Parser
+import Evaluator
+import System.Environment (getArgs)
 
+
+parse :: String -> BrainfuckSource
+parse x = case parseBrainfuck x of
+            (Left s) -> error s
+            (Right bs) -> bs
 
 main :: IO ()
 main = do
     argv <- getArgs
     case argv of
         (file:_) -> readFile file >>= runBrainfuck . parse
-                        where parse x = case parseBrainfuck x of
-                                            (Left s) -> error s
-                                            (Right bs) -> bs
         _        -> error "Bad input, it should be: brainfokt <filename.bf>"
 
